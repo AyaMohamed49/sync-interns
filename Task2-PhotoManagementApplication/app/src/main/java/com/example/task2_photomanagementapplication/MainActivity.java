9bad701143ca1f82ba2973ff00230dbbc793097d
aya560 <yokam2849@gmail.com>
2023-07-28 00:35:31 +0300

    update

--- Task2-PhotoManagementApplication/app/src/main/java/com/example/task2_photomanagementapplication/MainActivity.java
+++ Task2-PhotoManagementApplication/app/src/main/java/com/example/task2_photomanagementapplication/MainActivity.java
@@ -3,12 +3,33 @@ package com.example.task2_photomanagementapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.google.android.gms.cast.framework.media.ImagePicker;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView imageView;
+    FloatingActionButton floatingActionBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        imageView=findViewById(R.id.imageView);
+        floatingActionBtn=findViewById(R.id.floatingActionButton);
+
+        floatingActionBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+
+            }
+        });
+
     }
 }
